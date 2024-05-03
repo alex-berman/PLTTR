c654f1549bd8c9c5d0419eedadaec9f1b49ede91
Alex Berman <alexander.berman@gu.se>
2024-05-03 09:20:18 +0200

    WIP

--- example_isu.pl
+++ example_isu.pl
@@ -1,39 +1,13 @@
 % Game of fetch, corresponding to Cooper (2023, p.55)
-
-btype(ind).
-pred(pick_up, [ind, ind]).
-
-%% update_function(
-%% 	R,
-%% 	rec_type({agenda: singleton_type(list_type(type), [])}),
-%% 	rec_type({agenda: singleton_type(list_type(type), [
-%% 					     rec_type({e: pick_up(a, c))
-
 function(
-	_R:[agenda=[]:list(rec_type)],
+	[agenda=[]:list(rec_type)],
 	[agenda=[[e:pick_up(a, c)]]:list(rec_type)]).
 
-
-% Corresponds to Cooper (2023, p. 61), 54
-%% action_rule(
-%% 	S:[agenda:[fst:rec_type,
-%% 		   rst:list(rec_type)]],
-%% 	create(S/agenda/fst)).
-
 % Corresponds to Cooper (2023, p. 61), 55b
-action_rule(
-	[function(V:T, Type), CurrentState:T],
-	[apply(function(V:T, Type), CurrentState, ResultingType), next_state:ResultingType]).
+has_type(s(N), TypeN) :-
+    N >= 1,
+    N_minus_1 is N - 1,
+    has_type(s(N_minus_1), T),
+    function(T, TypeN).
 
-
-
-get_next_state :-
-    action_rule(Preconds, Effects),
-    test_preconds(Preconds),
-    apply_effects(Effects).
-
-
-has_type(current_state, [agenda=[]:list(rec_type)]).
-
-
-:- get_next_state, has_type(next_state, T), write('Next state: '), write(T), nl.
+has_type(s(0), [agenda=[]:list(rec_type)]).
