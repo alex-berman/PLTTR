e79d0b2c9f1584ea9d033e89056955517037a52a
Alex Berman <alexander.berman@gu.se>
2024-05-03 10:28:17 +0200

    WIP

--- example_isu.pl
+++ example_isu.pl
@@ -1,13 +1,28 @@
+:- use_module(ttr).
+
 % Game of fetch, corresponding to Cooper (2023, p.55)
 function(
-	[agenda=[]:list(rec_type)],
-	[agenda=[[e:pick_up(a, c)]]:list(rec_type)]).
+    [agenda=[]:list(rec_type)],
+    [agenda=[[e:pick_up(a, c)]]:list(rec_type)]).
+function(
+    [agenda=[[e:pick_up(a, c)]]:list(rec_type)],
+    function(
+	[e:pick_up(a, c)],
+	[agenda=[[e:attract_attention(a, b)]]:list(rec_type)])).
+
+% Corresponds to Cooper (2023, p. 61), 54
+%has_type(first_on_agenda, T) :-
+%    has_type(s(_), [agenda=),
+    
 
 % Corresponds to Cooper (2023, p. 61), 55b
-has_type(s(N), TypeN) :-
+s(N, V) :-
     N >= 1,
-    N_minus_1 is N - 1,
-    has_type(s(N_minus_1), T),
-    function(T, TypeN).
+    N_prev is N - 1,
+    s(N_prev, S_prev),
+    has_type(S_prev, T_prev),
+    function(T_prev, T),
+    T \= function(_, _),
+    has_type(V, T).
 
-has_type(s(0), [agenda=[]:list(rec_type)]).
+s(0, [agenda=[]]).
---
+++ ttr.pl
@@ -0,0 +1,9 @@
+:- module(ttr, [create/2, has_type/2]).
+
+% create(?Type, ?Witness)
+create([], []).
+create([L=V:list(_)|TypeTail], [L=V|WitnessTail]) :-
+    create(TypeTail, WitnessTail).
+
+has_type(Witness, Type) :-
+    create(Type, Witness).
